bba1dae1502d6a44d33bb0ab0b85f0770e8c9f11
Alexander Kern <alexander.t.kern@gmail.com>
2025-10-11 10:42:11 -0500

    PokeAPI location area fetch and types

---
+++ src/pokeapi.ts
@@ -0,0 +1,35 @@
+export class PokeAPI {
+  private static readonly baseURL = "https://pokeapi.co/api/v2";
+
+  constructor() {}
+
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    const url = pageURL ?? `${PokeAPI.baseURL}/location-area` 
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("invalid page URL");
+    }
+    return response.json();
+  }
+
+  async fetchLocation(locationName: string): Promise<Location> {
+    const url = `${PokeAPI.baseURL}/location-area/${locationName}` 
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error("invalid location");
+    }
+    return response.json();
+  }
+}
+
+export type ShallowLocations = {
+  count: string; 
+  next: string;
+  previous: string;
+  results: Location[];
+};
+
+export type Location = {
+  name: string;
+  url: string;
+};
